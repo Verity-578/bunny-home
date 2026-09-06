7fbf8556dc2cb2edcaee3a19cf46cccd90eeb30f
Verity-578 <Verity-578@users.noreply.github.com>
2026-09-07 01:49:46 +0800

    Remove top-level await for Netlify bundling

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,17 +1,7 @@
-let cachedHandler;
+import serverless from 'serverless-http';
 
-async function getHandler() {
-  if (!cachedHandler) {
-    const [{ default: serverless }, { createApp }] = await Promise.all([
-      import('serverless-http'),
-      import('../../bunny-home-backend/src/app.js'),
-    ]);
-    cachedHandler = serverless(createApp());
-  }
-  return cachedHandler;
-}
+import { createApp } from '../../bunny-home-backend/src/app.js';
 
-exports.handler = async (event, context) => {
-  const handler = await getHandler();
-  return handler(event, context);
-};
+const app = createApp();
+
+export const handler = serverless(app);
