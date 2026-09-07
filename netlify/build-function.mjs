d7edf53129c329d441bb44198d06fea347dd597f
Verity-578 <zhuqingyin578@gmail.com>
2026-09-07 13:09:45 +0800

    Change entry point from api.cjs to api.js

--- netlify/build-function.mjs
+++ netlify/build-function.mjs
@@ -5,7 +5,7 @@ import { build } from 'esbuild';
 const root = process.cwd();
 
 await build({
-  entryPoints: [path.join(root, 'netlify/functions/api.cjs')],
+  entryPoints: [path.join(root, 'netlify/functions/api.js')],
   bundle: true,
   platform: 'node',
   format: 'cjs',
